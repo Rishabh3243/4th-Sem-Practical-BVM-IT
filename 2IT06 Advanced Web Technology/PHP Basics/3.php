59d5f507455d65a0a7464c2144b1f3d54d57a79e
Rishabh Bharatbhai Parmar <rishabhpar7@gmail.com>
2023-03-05 15:33:47 +0530

    git

--- 2IT06 Advanced Web Technology/PHP Basics/3.php
+++ 2IT06 Advanced Web Technology/PHP Basics/3.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html>
 <body>
+<center>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<h1> POST method </h1><br>
+
   Name: <input type="text" name="fname">
   <input type="submit">
 </form>
-
+</center>
 <?php
+echo "<center>";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = htmlspecialchars($_REQUEST['fname']);
-    if (empty($name)) {
-        echo "Name is empty";
+    $n = htmlspecialchars($_REQUEST['fname']);
+    if (empty($n)) {
+        echo "<br>Name is empty";
     } else {
-        echo $name;
+        echo "<br>Your Name is : ".$n;
     }
 }
+echo "</center>";
 ?>
 
 </body>
